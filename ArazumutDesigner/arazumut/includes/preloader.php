6daf34ed231ac29fecfccde344118d050a7c3909
Umut <arazumut349@gmail.com>
2025-04-26 18:25:29 +0300

    PHP dosyaları için preloader ve menü bileşenleri ayrı dosyalara alındı, HTML şablonları PHP ile daha iyi entegre edildi.

---
+++ ArazumutDesigner/arazumut/includes/preloader.php
@@ -0,0 +1,15 @@
+<div class="mil-preloader">
+            <div class="mil-preloader-animation">
+                <div class="mil-pos-abs mil-animation-1">
+                    <p class="mil-h3 mil-muted mil-thin">Umut</p>
+                    <p class="mil-h3 mil-muted">Araz</p>
+                    <p class="mil-h3 mil-muted mil-thin">Designer</p>
+                </div>
+                <div class="mil-pos-abs mil-animation-2">
+                    <div class="mil-reveal-frame">
+                        <p class="mil-reveal-box"></p>
+                        <p class="mil-h3 mil-muted mil-thin">UmutAraz.com</p>
+                    </div>
+                </div>
+            </div>
+        </div>
